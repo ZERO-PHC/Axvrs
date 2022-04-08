f60ae52c610d3d73096aa03f2dc9001a80600bed
ZER0-PHC <101215453+ZER0-PHC@users.noreply.github.com>
2022-04-08 15:39:17 -0500

    chaiinnngeee

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Logo } from '@pmndrs/branding'
 import './styles.css'
 import { Text } from '@react-three/drei'
 import Rive from 'rive-react'
+import { useSpring, animated } from 'react-spring'
 
 import App from './App'
 
+const Card = ({ cardStyles }) => {
+  return (
+    <animated.main
+      style={{
+        background: '0 4px 30px rgba(0, 0, 0, 0.1)',
+        backdropFilter: 'blur(8px)',
+        webkitBackdropFilter: 'blur(8px)',
+        height: '56vh',
+        width: '60vw',
+        border: '2px solid silver',
+        ...cardStyles
+      }}>
+      card
+    </animated.main>
+  )
+}
+
 function Overlay() {
+  const [scrolled, setScrolled] = useState(false)
+  const styles = useSpring({
+    opacity: scrolled ? 0 : 1,
+    top: scrolled ? '10%' : '50%'
+  })
+  const cardStyles = useSpring({
+    // opacity: scrolled ? 1 : 0,
+    top: scrolled ? '25%' : '100%'
+  })
+
   return (
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-        <div style={{ position: 'absolute', top: '50%', left: '23%', transform: 'translate3d(-50%,-50%,0)' }}>
+        <animated.div style={{ position: 'absolute', left: '23%', transform: 'translate3d(-50%,-50%,0)', ...styles }}>
           <h1 style={{ margin: 0, padding: 0, fontSize: '3em', fontWeight: 500, letterSpacing: '-0.05em' }}>
             Welcome <br /> to the <br /> Axoverse
           </h1>
-        </div>
+        </animated.div>
+        <animated.div style={{ position: 'absolute', left: '50%', transform: 'translate(-50%, 0)', ...cardStyles }}>
+          <Card cardStyles={cardStyles} />
+        </animated.div>
         <div style={{ position: 'absolute', top: 55, left: '10%', fontSize: '13px' }}>LOGO—</div>
         <nav style={{ display: 'flex', position: 'absolute', top: 46, left: '50%', transform: 'translate(-50%, 0)', fontSize: '13px' }}>
           <div style={{ fontWeight: 900, fontSize: '1.4em' }}> MINTER</div>
@@ -45,6 +77,7 @@ function Overlay() {
         <img src={'https://res.cloudinary.com/do4mactw0/image/upload/v1649354231/icons_kk2hzt.svg'} alt="icons" />
       </div>
       <section
+        onClick={() => setScrolled(true)}
         style={{ height: '1.6rem', position: 'absolute', bottom: 30, left: '50%', transform: 'translate(-50%, 0)', fontSize: '13px' }}>
         <Rive src="scroll.riv" />
       </section>
